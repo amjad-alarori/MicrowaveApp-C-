9f7cd1acf46042df316e65e40755ea728636dbab
Samir Mokiem <s1158232@student.windesheim.nl>
2021-02-15 16:07:50 +0000

    Merged PR 2667: StateManager

--- MicrowaveApp/Form1.cs
+++ MicrowaveApp/Form1.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +15,82 @@ namespace MicrowaveApp
 {
     public partial class Form1 : Form
     {
+
+        StateManager _stateManager;
+
         public Form1()
         {
+            _stateManager = new StateManager();
             InitializeComponent();
+            UpdateView();
         }
+
+        public void UpdateView(object sender = null, EventArgs e = null)
+        {
+            String info = String.Format("Microwave: {0}\n\nLamp: {1}\n\nDoor: {2}", _stateManager._microwave.ToString(), _stateManager._door.ToString(), _stateManager._lamp.ToString());
+            this.StateTextBox.Text = info;
+
+            Console.Write(info);
+
+            foreach (Button btn in this.Controls.OfType<Button>().ToList())
+            {
+                this.Controls.Remove(btn);
+                btn.Dispose();
+            }
+
+            foreach (var permittedTrigger in _stateManager._microwave.GetPermittedTriggers().Select((value, i) => new { i, value }))
+            {
+                Button button = new Button
+                {
+                    Location = new Point(673, 12 + (23 * permittedTrigger.i)),
+                    Name = permittedTrigger.value.ToString(),
+                    Size = new Size(115, 23),
+                    Text = "Microwave: " + permittedTrigger.value.ToString(),
+                    UseVisualStyleBackColor = true,
+                };
+                button.Click += new EventHandler(HandleClick);
+                button.Click += new EventHandler(UpdateView);
+                this.Controls.Add(button);
+            }
+
+            foreach (var permittedTrigger in _stateManager._door.GetPermittedTriggers().Select((value, i) => new { i, value }))
+            {
+                Button button = new Button
+                {
+                    Location = new Point(673, 100 + (23 * permittedTrigger.i)),
+                    Name = permittedTrigger.value.ToString(),
+                    Size = new Size(115, 23),
+                    Text = "Door: " + permittedTrigger.value.ToString(),
+                    UseVisualStyleBackColor = true,
+                };
+                button.Click += new EventHandler(HandleClick);
+                button.Click += new EventHandler(UpdateView);
+                this.Controls.Add(button);
+            }
+
+            foreach (var permittedTrigger in _stateManager._lamp.GetPermittedTriggers().Select((value, i) => new { i, value }))
+            {
+                Button button = new Button
+                {
+                    Location = new Point(673, 200 + (23 * permittedTrigger.i)),
+                    Name = permittedTrigger.value.ToString(),
+                    Size = new Size(115, 23),
+                    Text = "Lamp: " + permittedTrigger.value.ToString(),
+                    // Lamp is an internal element that can't and shouldn't be interacted with by the end user
+                    Enabled = false,
+                    UseVisualStyleBackColor = true,
+                };
+                button.Click += new EventHandler(HandleClick);
+                button.Click += new EventHandler(UpdateView);
+                this.Controls.Add(button);
+            }
+        }
+
+        public void HandleClick(object sender, EventArgs e)
+        {
+            MethodInfo mi = _stateManager.GetType().GetMethod((sender as Button).Name);
+            mi.Invoke(_stateManager, null);
+        }
+
     }
 }
---
+++ MicrowaveApp/StateManager.cs
@@ -0,0 +1,173 @@
+﻿using Stateless;
+using System;
+
+namespace MicrowaveApp
+{
+
+    class StateManager
+    {
+        public enum MicrowaveState
+        {
+            Running,
+            Paused,
+            Stopped
+        }
+
+        public enum MicrowaveTriggers
+        {
+            Start,
+            Pause,
+            Stop
+        }
+
+        public enum DoorState
+        {
+            Open,
+            Closed
+        }
+
+        public enum DoorTriggers
+        {
+            Open,
+            Close
+        }
+
+        public enum LampState
+        {
+            On,
+            Off
+        }
+
+        public enum LampTriggers
+        {
+            TurnOn,
+            TurnOff
+        }
+
+        // Todo:: Check var name
+        public StateMachine<MicrowaveState, MicrowaveTriggers> _microwave;
+        public StateMachine<DoorState, DoorTriggers> _door;
+        public StateMachine<LampState, LampTriggers> _lamp;
+
+        public StateManager()
+        {
+            InitMicrowave();
+            InitDoor();
+            InitLamp();
+        }
+
+        void InitMicrowave()
+        {
+            _microwave = new StateMachine<MicrowaveState, MicrowaveTriggers>(MicrowaveState.Stopped);
+
+            _microwave.Configure(MicrowaveState.Running)
+                .Permit(MicrowaveTriggers.Stop, MicrowaveState.Stopped)
+                .Permit(MicrowaveTriggers.Pause, MicrowaveState.Paused);
+
+            _microwave.Configure(MicrowaveState.Paused)
+                .PermitIf(MicrowaveTriggers.Start, MicrowaveState.Running, () => _door.IsInState(DoorState.Closed))
+                .Permit(MicrowaveTriggers.Stop, MicrowaveState.Stopped);
+
+            _microwave.Configure(MicrowaveState.Stopped)
+                .PermitIf(MicrowaveTriggers.Start, MicrowaveState.Running, () => _door.IsInState(DoorState.Closed));
+        }
+
+        void InitDoor()
+        {
+            _door = new StateMachine<DoorState, DoorTriggers>(DoorState.Closed);
+
+            _door.Configure(DoorState.Closed)
+                .Permit(DoorTriggers.Open, DoorState.Open);
+
+            _door.Configure(DoorState.Open)
+                .Permit(DoorTriggers.Close, DoorState.Closed);
+        }
+
+        void InitLamp()
+        {
+            _lamp = new StateMachine<LampState, LampTriggers>(LampState.Off);
+
+            _lamp.Configure(LampState.Off)
+                .Permit(LampTriggers.TurnOn, LampState.On);
+
+            _lamp.Configure(LampState.On)
+                .Permit(LampTriggers.TurnOff, LampState.Off);
+        }
+
+        // Todo:: Check if these functions are needed to hook to fire functions
+
+        // Start microwave
+        public void Start()
+        {
+            _microwave.Fire(MicrowaveTriggers.Start);
+            if (_lamp.CanFire(LampTriggers.TurnOff))
+            {
+                _lamp.Fire(LampTriggers.TurnOff);
+            }
+        }
+
+
+        // Pause microwave
+        public void Pause()
+        {
+            _microwave.Fire(MicrowaveTriggers.Pause);
+        }
+
+
+        // Stop microwave
+        public void Stop()
+        {
+            _microwave.Fire(MicrowaveTriggers.Stop);
+            if (_lamp.CanFire(LampTriggers.TurnOff))
+            {
+                _lamp.Fire(LampTriggers.TurnOff);
+            }
+        }
+
+        // Open door
+        public void Open()
+        {
+            _door.Fire(DoorTriggers.Open);
+
+            // Check if lamp can be turn on. If not, the lamp is already on
+            if (_lamp.CanFire(LampTriggers.TurnOn))
+            {
+                _lamp.Fire(LampTriggers.TurnOn);
+            }
+
+            if (_microwave.CanFire(MicrowaveTriggers.Pause))
+            {
+                _microwave.Fire(MicrowaveTriggers.Pause);
+            }
+        }
+
+
+        // Close door
+        public void Close()
+        {
+            _door.Fire(DoorTriggers.Close);
+            if (_lamp.CanFire(LampTriggers.TurnOff))
+            {
+                _lamp.Fire(LampTriggers.TurnOff);
+            }
+
+            if (_microwave.CanFire(MicrowaveTriggers.Start) && _microwave.IsInState(MicrowaveState.Paused))
+            {
+                _microwave.Fire(MicrowaveTriggers.Start);
+            }
+        }
+
+        // Turn on lamp owo
+        public void TurnOn()
+        {
+            _lamp.Fire(LampTriggers.TurnOn);
+        }
+
+        // Turn off lamp v~v
+        public void TurnOff()
+        {
+            _lamp.Fire(LampTriggers.TurnOff);
+        }
+
+    }
+}
