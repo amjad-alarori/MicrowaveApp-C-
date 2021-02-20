2c30e337d981bc6eda3a4f510a631b9b207dc136
hudavid17 <hudavid17@gmail.com>
2021-02-20 12:31:40 +0100

    Changed MicrowaveApp

---
+++ MicrowaveApp/MicrowaveLamp.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicrowaveApp
+{
+    class MicrowaveLamp:Microwave
+    {
+        public enum MicrowaveState
+        {
+            Running,
+            Paused,
+            Stopped
+        }
+
+        public enum MicrowaveTriggers
+        {
+            Start,
+            Pause,
+            Stop
+        }
+
+        public enum DoorState
+        {
+            Open,
+            Closed
+        }
+
+        public enum DoorTriggers
+        {
+            Open,
+            Close
+        }
+
+        public enum LampState
+        {
+            On,
+            Off
+        }
+
+        public enum LampTriggers
+        {
+            TurnOn,
+            TurnOff
+        }
+    }
+}
