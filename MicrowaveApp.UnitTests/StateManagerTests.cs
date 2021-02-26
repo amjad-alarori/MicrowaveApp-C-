74cd110eb49d932550802f5151f7bdd679c6e038
hudavid17 <hudavid17@gmail.com>
2021-02-27 00:04:41 +0100

    getting ready to write tests

--- MicrowaveApp.UnitTests/StateManagerTests.cs
+++ MicrowaveApp.UnitTests/StateManagerTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Windows.Forms;
 
 namespace MicrowaveApp.UnitTests
 {
@@ -8,7 +9,10 @@ namespace MicrowaveApp.UnitTests
         [TestMethod]
         public void Testdefaultstate()
         {
-
+            Timer timer = new Timer();
+            TextBox textBox = new TextBox();
+            TimerWrapper timerWrapper = new TimerWrapper(timer, textBox);
+            StateManager stateManager = new StateManager(timerWrapper);
         }
     }
 }
