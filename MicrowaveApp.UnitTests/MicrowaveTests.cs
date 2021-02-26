3050c50fe4ebd97aabb7283cb740b4ae92d360df
Roderik Schiele <rorospits@hotmail.com>
2021-02-26 15:37:06 +0100

    Unit tests microwave done and tested

--- MicrowaveApp.UnitTests/MicrowaveTests.cs
+++ MicrowaveApp.UnitTests/MicrowaveTests.cs
@@ -35,7 +35,7 @@ namespace MicrowaveApp.UnitTests
         }
 
         [TestMethod]
-        public void TestMicrowaveStopToStopTrigger()
+        public void TestMicrowaveStoppedToStopTrigger()
         {
             System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
             TextBox textBox = new TextBox();
@@ -47,13 +47,201 @@ namespace MicrowaveApp.UnitTests
         }
 
         [TestMethod]
-        public void TestMicrowaveStopToPauseTrigger()
+        public void TestMicrowaveStoppedToPauseTrigger()
         {
             System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
             TextBox textBox = new TextBox();
             TimerWrapper timerWrapper = new TimerWrapper(timer, textBox);
             StateManager stateManager = new StateManager(timerWrapper);
             Microwave microwave = stateManager.Microwave;
+            Assert.ThrowsException<System.InvalidOperationException>(() => microwave.StateMachine.Fire(MicrowaveTriggers.Pause));
+        }
+
+        [TestMethod]
+        public void TestMicrowaveRunningToStartTrigger()
+        {
+            System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+            TextBox textBox = new TextBox();
+            TimerWrapper timerWrapper = new TimerWrapper(timer, textBox);
+            StateManager stateManager = new StateManager(timerWrapper);
+            Microwave microwave = stateManager.Microwave;
+            try
+            {
+                microwave.StateMachine.Fire(MicrowaveTriggers.Start);
+            }
+            catch (Exception)
+            {
+                // On entry failed. But state changed to Running anyway
+            }
+
+            try
+            {
+                microwave.StateMachine.Fire(MicrowaveTriggers.Start);
+            }
+            catch (Exception)
+            {
+                // On entry failed. But state changed to Running anyway
+            }
+
+            // We expect that we cannot start the microwave when already in running state
+            Assert.ThrowsException<System.InvalidOperationException>(() => microwave.StateMachine.Fire(MicrowaveTriggers.Start));
+        }
+
+        [TestMethod]
+        public void TestMicrowaveRunningToStopTrigger()
+        {
+            System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+            TextBox textBox = new TextBox();
+            TimerWrapper timerWrapper = new TimerWrapper(timer, textBox);
+            StateManager stateManager = new StateManager(timerWrapper);
+            Microwave microwave = stateManager.Microwave;
+            try
+            {
+                microwave.StateMachine.Fire(MicrowaveTriggers.Start);
+            }
+            catch (Exception)
+            {
+                // On entry failed. But state changed to Running anyway
+            }
+           
+            try
+            {
+                microwave.StateMachine.Fire(MicrowaveTriggers.Stop);
+            }
+            catch (Exception )
+            {
+                // On entry failed. But state changed to Running anyway
+            }
+            Assert.IsTrue(microwave.StateMachine.IsInState(MicrowaveStates.Stopped)); 
+        }
+
+        [TestMethod]
+        public void TestMicrowaveRunningToPauseTrigger()
+        {
+            System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+            TextBox textBox = new TextBox();
+            TimerWrapper timerWrapper = new TimerWrapper(timer, textBox);
+            StateManager stateManager = new StateManager(timerWrapper);
+            Microwave microwave = stateManager.Microwave;
+            try
+            {
+                microwave.StateMachine.Fire(MicrowaveTriggers.Start);
+            }
+            catch (Exception)
+            {
+                // On entry failed. But state changed to Running anyway
+            }
+
+            try
+            {
+                microwave.StateMachine.Fire(MicrowaveTriggers.Pause);
+            }
+            catch (Exception)
+            {
+                // On entry failed. But state changed to Running anyway
+            }
+            Assert.IsTrue(microwave.StateMachine.IsInState(MicrowaveStates.Paused));
+        }
+
+        [TestMethod]
+        public void TestMicrowavePausedtoStartTrigger()
+        {
+            System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+            TextBox textBox = new TextBox();
+            TimerWrapper timerWrapper = new TimerWrapper(timer, textBox);
+            StateManager stateManager = new StateManager(timerWrapper);
+            Microwave microwave = stateManager.Microwave;
+            try
+            {
+                microwave.StateMachine.Fire(MicrowaveTriggers.Start);
+            }
+            catch (Exception)
+            {
+                // On entry failed. But state changed to Running anyway
+            }
+
+            try
+            {
+                microwave.StateMachine.Fire(MicrowaveTriggers.Pause);
+            }
+            catch (Exception)
+            {
+                // On entry failed. But state changed to Paused anyway
+            }
+            try
+            {
+                microwave.StateMachine.Fire(MicrowaveTriggers.Start);
+            }
+            catch (Exception)
+            {
+                // On entry failed. But state changed to Running anyway
+            }
+            Assert.IsTrue(microwave.StateMachine.IsInState(MicrowaveStates.Running));
+        }
+
+        [TestMethod]
+        public void TestMicrowavePausedToStopTrigger()
+        {
+            System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+            TextBox textBox = new TextBox();
+            TimerWrapper timerWrapper = new TimerWrapper(timer, textBox);
+            StateManager stateManager = new StateManager(timerWrapper);
+            Microwave microwave = stateManager.Microwave;
+            try
+            {
+                microwave.StateMachine.Fire(MicrowaveTriggers.Start);
+            }
+            catch (Exception)
+            {
+                // On entry failed. But state changed to Running anyway
+            }
+
+            try
+            {
+                microwave.StateMachine.Fire(MicrowaveTriggers.Pause);
+            }
+            catch (Exception)
+            {
+                // On entry failed. But state changed to Paused anyway
+            }
+            try
+            {
+                microwave.StateMachine.Fire(MicrowaveTriggers.Stop);
+            }
+            catch (Exception)
+            {
+                // On entry failed. But state changed to Stopped anyway
+            }
+            Assert.IsTrue(microwave.StateMachine.IsInState(MicrowaveStates.Stopped));
+        }
+
+        [TestMethod]
+        public void TestMicrowavePausedToPauseTrigger()
+        {
+            System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+            TextBox textBox = new TextBox();
+            TimerWrapper timerWrapper = new TimerWrapper(timer, textBox);
+            StateManager stateManager = new StateManager(timerWrapper);
+            Microwave microwave = stateManager.Microwave;
+            try
+            {
+                microwave.StateMachine.Fire(MicrowaveTriggers.Start);
+            }
+            catch (Exception)
+            {
+                // On entry failed. But state changed to Running anyway
+            }
+
+            try
+            {
+                microwave.StateMachine.Fire(MicrowaveTriggers.Pause);
+            }
+
+            catch (Exception)
+            {
+                // On entry Failed. But state changed to Paused anyway
+            }
+           
             Assert.ThrowsException<System.InvalidOperationException>(() => microwave.StateMachine.Fire(MicrowaveTriggers.Pause));
         }
     }
