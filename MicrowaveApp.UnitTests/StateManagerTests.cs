95799e24be6d08a0605c6a576a92dcc587a6875a
hudavid17 <hudavid17@gmail.com>
2021-02-27 13:24:06 +0100

    Finished my tests

--- MicrowaveApp.UnitTests/StateManagerTests.cs
+++ MicrowaveApp.UnitTests/StateManagerTests.cs
@@ -7,12 +7,53 @@ namespace MicrowaveApp.UnitTests
     public class StateManagerTests
     {
         [TestMethod]
-        public void Testdefaultstate()
+        public void TestStoppedState()
         {
             Timer timer = new Timer();
             TextBox textBox = new TextBox();
             TimerWrapper timerWrapper = new TimerWrapper(timer, textBox);
             StateManager stateManager = new StateManager(timerWrapper);
+            Assert.IsTrue(stateManager.Microwave.StateMachine.IsInState(MicrowaveStates.Stopped));
+        }
+
+        [TestMethod]
+        public void TestPausedState()
+        {
+            Timer timer = new Timer();
+            TextBox textBox = new TextBox();
+            TimerWrapper timerWrapper = new TimerWrapper(timer, textBox);
+            StateManager stateManager = new StateManager(timerWrapper);
+            Assert.IsFalse(stateManager.Microwave.StateMachine.IsInState(MicrowaveStates.Paused));
+        }
+
+        [TestMethod]
+        public void TestRunningState()
+        {
+            Timer timer = new Timer();
+            TextBox textBox = new TextBox();
+            TimerWrapper timerWrapper = new TimerWrapper(timer, textBox);
+            StateManager stateManager = new StateManager(timerWrapper);
+            Assert.IsFalse(stateManager.Microwave.StateMachine.IsInState(MicrowaveStates.Running));
+        }
+
+        [TestMethod]
+        public void TestDoorOpen()
+        {
+            Timer timer = new Timer();
+            TextBox textBox = new TextBox();
+            TimerWrapper timerWrapper = new TimerWrapper(timer, textBox);
+            StateManager stateManager = new StateManager(timerWrapper);
+            Assert.IsFalse(stateManager.Door.StateMachine.IsInState(DoorStates.Open));
+        }
+
+        [TestMethod]
+        public void TestDoorCLosed()
+        {
+            Timer timer = new Timer();
+            TextBox textBox = new TextBox();
+            TimerWrapper timerWrapper = new TimerWrapper(timer, textBox);
+            StateManager stateManager = new StateManager(timerWrapper);
+            Assert.IsTrue(stateManager.Door.StateMachine.IsInState(DoorStates.Closed));
         }
     }
 }
